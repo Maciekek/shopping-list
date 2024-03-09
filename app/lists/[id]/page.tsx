4b1b8a73ff5808d7d29955c69b242e85e6157a68
maciej.kucharski <maciej.kucharski@senti1.com>
2024-03-09 19:37:46 +0100

    test

--- app/lists/[id]/page.tsx
+++ app/lists/[id]/page.tsx
@@ -1,6 +1,6 @@
+'use server'
 import { getList } from '@/app/lists/actions/list';
 import List from '@/app/lists/[id]/List';
-
 
 export default async function ListPage({ params }: { params: { id: string } }) {
   const listDetails: any = await getList(params.id);
