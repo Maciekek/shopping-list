c96e01e6a2246ecb5484644ac99b6bc00f13beb8
maciej.kucharski <maciej.kucharski@senti1.com>
2024-08-20 14:01:08 +0200

    wip reordering list

--- components/molecules/sortableList/SortableList.tsx
+++ components/molecules/sortableList/SortableList.tsx
@@ -36,7 +36,7 @@ const dropAnimationConfig: DropAnimation = {
   sideEffects: defaultDropAnimationSideEffects({
     styles: {
       active: {
-        opacity: '0.4'
+        opacity: '0.1'
       }
     }
   })
